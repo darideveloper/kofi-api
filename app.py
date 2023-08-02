5859217763e25e26d8931fd28e69e038e7dcf330
daridev <darideveloper@gmail.com>
2023-08-01 21:46:38 -0600

    feat: save donations in google sheets

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 import json
-from datetime import datetime
+from datetime import datetime as dt
 from flask import Flask, request
 from dotenv import load_dotenv
+from spreadsheets.google_sheets import SheetsManager
 
 load_dotenv ()
 
 app = Flask(__name__)
 KOFI_TOKEN = os.getenv("KOFI_TOKEN")
+GOOGLE_SHEETS = os.getenv("GOOGLE_SHEETS")
 
 @app.post("/")
 def home():
@@ -18,7 +20,7 @@ def home():
     res_token = form_data["verification_token"]
     timestamp = form_data["timestamp"]
     res_type = form_data["type"]
-    from_name = form_data["from_name"]
+    user_name = form_data["from_name"]
     message = form_data["message"]
     amount = form_data["amount"]
     email = form_data["email"]
@@ -31,8 +33,25 @@ def home():
         return ("Invalid token", 403)
     
     # Convert string to datetime
-    date = datetime.fromtimestamp (timestamp)
+    datetime = dt.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+    date = datetime.strftime("%d/%m/%Y")
+    time = datetime.strftime("%H:%M:%S")
     
+    # Connect to google sheets
+    current_folder = os.path.dirname(os.path.abspath(__file__))
+    creads_path = os.path.join (current_folder, "credentials.json")
+    sheets = SheetsManager (GOOGLE_SHEETS, creads_path, sheet_name="kofi")
+   
+    # Detect last row
+    current_row = sheets.get_rows_num () + 1 
+   
+    # Detect donation type
+    if res_type == "Donation":
+        # Write data in google sheets
+        sheets.write_data ([
+            [date, time, user_name, message, amount, email, currency]
+        ], row=current_row, column=1)
+ 
  
     return "<p>Hello, World!</p>"
 
---
+++ spreadsheets/google_sheets.py
@@ -0,0 +1,69 @@
+#! python3
+# Conect to google spreadsheets
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+class SheetsManager (): 
+    """ Class to conect to google shets and upload data"""
+
+    def __init__ (self, google_sheet_link, creds_path, sheet_name=None): 
+        """ Construtor of the class"""
+
+        # Read credentials
+        if not os.path.isfile (creds_path):
+            raise FileNotFoundError ("The credential file path is not correct")
+        
+        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scope)
+        client = gspread.authorize(creds)
+
+        # Conect to google sheet
+        sheet = client.open_by_url(google_sheet_link)
+
+        # Set the sheet 1 as worksheet
+        if sheet_name:
+            self.worksheet = sheet.worksheet(sheet_name)
+        else:
+            self.worksheet = sheet.sheet1
+
+    def write_cell (self, value, row=1, column=1):
+        """ Write data in specific cell 
+        """
+        self.worksheet.update_cell(row, column, value)
+
+    def write_data (self, data, row=1, column=1): 
+        """ Write list of data in the worksheet"""
+        
+        # check if data exist
+        if not data: 
+            print ("THERE IS NO NEW INFORMATION TO WRITE IN THE FILE.")
+        else:
+            print ("Writing information on spreadsheet...")
+
+            # Loop for each row of data
+            for row_data in data: 
+
+                # Set the position of the next row. Omit the header
+                row_index = data.index(row_data) + row
+                
+                for cell in row_data:
+                    column_index = row_data.index (cell) + column
+                    
+                    self.write_cell (cell, row_index, column_index)
+
+    def get_data (self): 
+        """ Read all records of the sheet"""
+
+        records = self.worksheet.get_all_records()
+        return records
+
+    def get_rows_num (self) -> int: 
+        """ Get number of the rows in use """
+
+        return len(self.worksheet.col_values(1))
+    
+    def get_cols_num (self) -> int: 
+        """ Get number of the columns in use """
+
+        return len(self.worksheet.rows_values(1))
